6ea49b0dc129fc30f3e42071fcb864daa665bfe3
Anton <Anton_fast@hotmail.com>
2016-08-28 02:45:25 +0200

    Villkor page

---
+++ wp-content/themes/proteingrot/templates/villkor.php
@@ -0,0 +1,64 @@
+<?php 
+
+/* Template Name: Villkor */ 
+
+get_header();
+?>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+    <p>§1 Allmänt<br />
+I alla priser ingår moms med12 procent på livsmedel och 25 procent på eventuell hälsokost. Vi reserverar oss för eventuella tryck- och skrivfel.</p>
+<p>Är du under 18år måste du ha målsmans tillstånd för att beställa.</p>
+<p>§2 Betalning<br />
+Vi erbjuder fyra olika betalningssätt via Klarna.</p>
+<p>Kortbetalning &#8211; Smidigt, enkelt och säkert. Kostnad: gratis<br />
+Du betalar tryggt med ditt kort. Vi använder den senaste tekniken för maximal säkerhet.</p>
+<p>Faktura &#8211; Handla nu, betala om 14 dagar. Kostnad: Gratis!<br />
+Betalningsvillkor 14 dagar. Vid försenad betalning utgår påminnelseavgift med det belopp som är tillåtet enligt lag f.n. 50 kr samt dröjsmålsränta med 24% + gällande referensränta. Förenklad kreditprövning utan kännedomskopia sker och leverans sker endast till folkbokförd adress.</p>
+<p>Postförskott &#8211; Om du vill betala när du hämtar ditt paket. Kostnad: 50kr (postens Väljer du postförskott får du lösa ut ditt paket mot betalning på ditt postkontor.</p>
+<p>Delbetalning/Konto &#8211; Dela upp din betalning<br />
+Du kan välja att delbetala din beställning med antingen klarna konto eller klarnas 6-månaders räntefria avbetalningsplan. Mer info nedan.</p>
+<p>Klarna konto<br />
+Betalsättet för dig som själv vill välja hur mycket du betalar varje månad. Men Klarna konto är mer än så. Förutom att du alltid får hem dina varor innan du betalar, samlar du alla dina köp på ett konto. Detta gäller även om du har handlat i flera olika butiker via Klarna konto. Det betyder en enda faktura, oavsett antalet köp.:</p>
+<p>Få alltid hem varan innan du betalar:<br />
+Betala från 50 kr/mån eller 1/24 av totalbeloppet:<br />
+Du behöver aldrig lämna ut kortuppgifter:<br />
+Alla dina köp samlas på ett konto och på en faktura:<br />
+Handla nu &#8211; betala i slutet av nästkommande månad:<br />
+En aviavgift om 29 kr, oavsett antalet köp:<br />
+Du kan när som helst betala hela beloppet på en gång:<br />
+Vid ett köp om 10000 kr med Klarna Konto basvillkor är uppläggningsavgiften 0 kr. Den gällande årsräntan är 19,90 % vilket motsvarar en effektiv ränta om 29,22 %. Kreditköpspriset är 11458 kr. Antalet delbetalningar är 12 stycken och varje delbelopp är 955 kr. Exemplet förutsätter att avbetalning sker under ett års tid.: Vid köptillfället görs en kreditprövning som i vissa fall innebär att en kreditupplysning tas. Du kommer då få en kopia på kreditupplysningen per post.: Allmänna villkor hittar du här<br />
+För mer information se www.klarna.se</p>
+<p>6-månaders räntefri-avbetalning:<br />
+Klarna&#8217;s avbetalningsplan utan ränta.<br />
+Uppläggningsavgift: 195kr<br />
+Avi-avgift 29kr per månad.</p>
+<p>§3 Leverans<br />
+Vi skickar alla våra paket med posten varubrev vilket innebär att leveransen vid normal postgång levereras hem till dig. Normal leveranstid är 1-3 dagar.</p>
+<p>Frakten från oss är alltid gratis, men vid eventuell retur så behöver du själv stå för frakten. Vi skickar alla försändelser med varubrev vilket innebär att du får leveransen direkt hem i din brevlåda. Om det av någon anledning inte går att leverera det direkt hem till dig så får du får ett SMS till din mobil när ditt paket är framme på ditt utlämningsställe. Har du inte lämnat något mobilnummer får du en avi i din brevlåda. Utlämningsstället finns angivet i ditt SMS/avi.</p>
+<p>Paket som inte hämtas ut<br />
+Vi debiterar 199 kr för ej uthämtade paket för att täcka våra kostnader. Om du beställt en vara och ångrar dig efter vi skickat ut ditt paket måste du returnera paketet, om du inte ska bli debiterad 199kr.</p>
+<p>§4 Ångerrätt<br />
+Vi vill att alla är nöjda med våra artiklar, är du missnöjd med något kan du alltid skicka tillbaka artiklarna i obruten och ej skadad förpackning så får du pengarna tillbaka för köpet du gjort hos oss.</p>
+<p>§5 Säkerhet<br />
+&#8211; Kryptering<br />
+Du skall kunna känna dig trygg när du handlar hos oss, därför använder vi oss av SSL kryptering som är den vanligaste formen av kryptering.<br />
+SSL kryptering används ofta av banker.</p>
+<p>§7 Cookies<br />
+En cookie är en textfil som webbplatsen du besöker begär att få spara på din dator. Cookies används på många webbplatser för att ge besökare tillgång till olika funktioner. Informationen i en cookie är möjlig att använda för att följa en användares surfande. Enligt lagen om elektronisk kommunikation (25 juli 2003) ska alla som besöker en webbplats med cookies få information om:</p>
+<p>att webbplatsen innehåller cookies<br />
+vad dessa cookies används till<br />
+hur cookies kan användas<br />
+Denna webbplats använder två typer av cookies. Den ena typen sparar en fil permanent på din dator, dessa används för att exempelvis mäta hur du som besökare rör dig på hemsidan. Detta för att kunna erbjuda besökaren bättre tjänster och service. Informationen som sparas på din dator är endast ett unikt nummer. Den andra typen av cookies som vanligtvis används är &#8220;Session-Cookies&#8221;. Under tiden du är inne och besöker sidan delar vår webbserver ut en unik identifieringssträng för att inte blanda ihop dig med andra besökare. Detta för att kunna följa vad du gör på sidan, hålla koll på din inloggning och om placerat något i varukorgen, mäta trafik, exponering &#038; analys av marknadsföringsinsatser m.m. En &#8220;Session-Cookie&#8221; lagras aldrig permanent på din dator utan försvinner när du stänger din webbläsare. Ibland använder vi cookies från tredjepart för att exempelvis analysera hur vi kan optimera vår webbplats eller för att visa annonser och rekommendera produkter för dig som besökt hemsidan när du besöker andra hemsidor på internet. Om du inte accepterar användning av cookies på denna webbplats kan du ändra din webbläsares inställningar till att ej ta emot cookies. Vi hänvisar till respektive webbläsares tillverkare för närmare instruktioner.</p>
+<p>§6 Personuppgifter<br />
+Health &#038; Wealth Scandinavia AB är ansvarig för behandlingen av de personuppgifter som du som kund lämnar till oss. Dessa uppgifter kan användas för att ge dig information om proteingrot.se samt för nyhetsbrev och andra marknadsföringsändamål. Inga uppgifter lämnas vidare utan används endast utav proteingrot.se. Vill du rätta till några uppgifter eller bli borttagen ur registret är det bara att kontakta oss.</p>
+<p>§7 Force Majeure<br />
+Health &#038; Wealth Scandinavia AB är befriad från påföljd för underlåtenhet att fullgöra viss förpliktelse enligt ovan, om underlåtenheten har sin grund i befriande omständighet enligt nedan och omständigheten förhindrar, försvårar eller försenar fullgörandet. Som befriande omständighet skall anses bland annat. myndighetsåtgärd eller underlåtenhet, nytillkommen eller ändrad lagstiftning, konflikt på arbetsmarknaden, blockad, brand, översvämning, sabotage eller olyckshändelse av större omfattning. I force majeure ingår myndighetsbeslut som påverkar marknad och produkter negativt, till exempel restriktioner i indikation, varningstexter, försäljningsförbud etc., samt onormal nedgång i marknaden.</p>
+
+
+
+<?php get_footer(); ?>
