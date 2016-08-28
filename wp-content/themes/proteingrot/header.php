dea1b5bdd6b1089ac9f350d975a5e4a16318d139
Anton <Anton_fast@hotmail.com>
2016-08-28 02:23:52 +0200

    header

--- wp-content/themes/proteingrot/header.php
+++ wp-content/themes/proteingrot/header.php
@@ -30,9 +30,9 @@
    <span class="fa fa-instagram" aria-hidden="true"></span>
    <span class="user">
    <?php if ( is_user_logged_in() ) {
-    echo '<span class="button">Logga ut</span>';
+    echo '<a href="'. wp_logout_url( get_permalink( woocommerce_get_page_id( 'myaccount' ) ) ) .'"><span class="button">Logga ut</span></a>';
 } else {
-    echo '<span class="button">Logga in</span>';
+    echo '<a href="'. wp_logout_url( get_permalink( woocommerce_get_page_id( 'myaccount' ) ) ) .'"><span class="button">Logga in</span></a>';
 } ?>
    </span>
   </div>
