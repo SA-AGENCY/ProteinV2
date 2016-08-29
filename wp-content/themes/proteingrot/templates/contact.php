dc79540ad51c352060fc2c78d68a2da21858606a
Anton <Anton_fast@hotmail.com>
2016-08-29 11:01:35 +0200

    Contact template

---
+++ wp-content/themes/proteingrot/templates/contact.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Template Name: Kontakt
+*/
+?>
+<?php get_header(); ?>
+ <div class="static-spacer"></div>
+<br>
+<br>
+<section style="background-color:#74af26;">
+ <div class="wrapper" style="text-align:center;">
+	<h2 style="color:#fff;"> Kontakt! </h2>
+
+	<p style="color:#fff;">VILL DU VETA MER OM ELLER BLI
+ÅTERFÖRSÄLJARE AV VÅR GRÖT? <br>
+
+KONTAKTA OSS PÅ:<br>
+08-7108000<br>
+info@proteingrot.se</p>	
+
+
+ </div>
+</section>
+
+
+<section class="footerLast" style="background-color:#74AF27;">
+ <img src="http://woowho.se/wp-content/themes/protein/images/SF_pos.svg" style="width:350px;height:350px;"><br>
+ Proteingröt drivs av SuperFuel AB<br>
+ All rights reserved Superfuel AB 2016
+</section>
+<?php get_footer(); ?>
