41208307716216e6254c5fb01b64e6879b594021
Anton <Anton_fast@hotmail.com>
2016-08-29 18:32:55 +0200

    Ändrat tillbaka translate

--- wp-content/plugins/woocommerce/templates/cart/proceed-to-checkout-button.php
+++ wp-content/plugins/woocommerce/templates/cart/proceed-to-checkout-button.php
@@ -24,5 +24,5 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <a href="<?php echo esc_url( wc_get_checkout_url() ) ;?>" class="checkout-button button alt wc-forward">
-	<?php echo __( 'Fortsätt till kassan', 'woocommerce' ); ?>
+	<?php echo __( 'Proceed to Checkout', 'woocommerce' ); ?>
 </a>
